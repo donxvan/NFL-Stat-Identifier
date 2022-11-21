32549136b79db41c5befe0e3bc12c591b19d797d
DreyaWilborn <111436080+DreyaWilborn@users.noreply.github.com>
2022-11-21 14:01:58 -0500

    Add files via upload
    add tracker code

---
+++ NFL Stat Identifier.py
@@ -0,0 +1,6 @@
+
+
+BASE_URL = 'https://www.pro-football-reference.com{0}'
+PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
+PLAYER_PROFILE_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
+PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
