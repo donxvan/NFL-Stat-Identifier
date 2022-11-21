dba6f7189dc051b57303b336a889f4ae27c9c098
DreyaWilborn <111436080+DreyaWilborn@users.noreply.github.com>
2022-11-21 15:06:30 -0500

    Update NFL Stat Identifier.py
    URL from player stat site, learning how to code player stats, such as recieving, passing ,tackles, etc. 
    Using other repository examples and youtube for help

--- NFL Stat Identifier.py
+++ NFL Stat Identifier.py
@@ -2,3 +2,12 @@ BASE_URL = 'https://www.pro-football-reference.com{0}'
 PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
 PLAYER_PROFILE_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
 PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
+
+
+
+class NFLStats(object):
+  
+class NFLPassingStats(NFLStats):
+    @staticmethod
+    def keys(): 
+        return [ 'id', 'player_id', 'name', "side", 'ints', 'cmp', 'twoptm', 'yds', 'att', 'tds', "twopta"]
