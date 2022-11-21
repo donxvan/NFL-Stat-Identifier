927f3b34fc6c7195867801fc03dd31c1e30ab8d2
DreyaWilborn <111436080+DreyaWilborn@users.noreply.github.com>
2022-11-21 15:20:10 -0500

    Update NFL Stat Identifier.py
    another code update, from examples using other repositories and youtube

--- NFL Stat Identifier.py
+++ NFL Stat Identifier.py
@@ -1,13 +1,25 @@
+import 
+from  import 
+from import 
+import time
+import shutil
+import re
+import os
+import json
+import string
+import glob
+
+
 BASE_URL = 'https://www.pro-football-reference.com{0}'
 PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
 PLAYER_PROFILE_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
 PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
 
-
-
-class NFLStats(object):
+class 
   
-class NFLPassingStats(NFLStats):
-    @staticmethod
+class NFLPassingStats(NFLStats):  
     def keys(): 
         return [ 'id', 'player_id', 'name', "side", 'ints', 'cmp', 'twoptm', 'yds', 'att', 'tds', "twopta"]
+class NFLReceivingStats(NFLStats):
+    def keys(): 
+        return [ 'id', 'player_id', 'name', "side", 'twoptm', 'rec', 'yds', 'lngtd', 'tds', 'lng', 'twopta']
